b10f3e3306a538819771e2cab7919471b57c8798
dandevweb <dandevweb@gmail.com>
2023-01-27 20:06:50 -0300

    create admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')
+    ->middleware(['auth', 'permission:view admin dashboard'])
+	->name('admin.')
+    ->group(static function () {
+        Route::get('/', [HomeController::class, 'index'])->name('home.index');
+    });
